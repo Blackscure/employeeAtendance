31f4a7fddaae471541784f204a796f2864530104
Blackscure <Blackscure>
2020-10-27 09:02:11 +0300

    tables created in index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<?php
+ 
+
+?>
+<table width="100%" border="1" cellspacing="0" cellpadding="0" style="border-collapse:collapse;">
+<thead>
+  <tr>
+    <td width="25">No</td>
+    <td>UID</td>
+    <td>ID</td>
+    <td>Name</td>
+    <td>Role</td>
+    <td>Password</td>
+  </tr>
+</thead>
+<tbody>
+<?php
+ 
+  ?>
+  <tr>
+    <td align="right"><?php echo $no; ?></td>
+    <td><?php echo $key; ?></td>
+    <td><?php echo $user[0]; ?></td>
+    <td><?php echo $user[1]; ?></td>
+    <td><?php echo $user[2]; ?></td>
+    <td><?php echo $user[3]; ?></td>
+  </tr>
+  <?php
+}
+?>
+</tbody>
+</table>
+<?php
+ 
+
+?>
